49c97cdf98649873740a1f6c9e7ed4696d8b454c
jakevollkommer <7663746+jakevollkommer@users.noreply.github.com>
2021-10-16 12:06:33 +1000

    always show hidden files in finder

--- setup_mac_preferences.zsh
+++ setup_mac_preferences.zsh
@@ -2,6 +2,9 @@
 
 echo "\n<<< Starting Mac Preferences Setup >>>\n"
 
+echo "Always show hidden files in Finder ..."
+defaults write com.apple.finder AppleShowAllFiles -boolean true
+
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
 echo "Using list view in all Finder windows by default ..."
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
@@ -15,79 +18,80 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 echo "Show ~/Library folder ..."
 chflags nohidden ~/Library
 
-echo "Disable the “Are you sure you want to open this application?” dialog"
+echo "Disable the “Are you sure you want to open this application?” dialog ..."
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-echo "Disables \"Scroll direction: natural\" in favor of old and correct style."
+echo "Disables \"Scroll direction: natural\" in favor of old and correct style ..."
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-echo "Set a blazingly fast keyboard repeat rate"
+echo "Set a blazingly fast keyboard repeat rate ..."
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-echo "Automatically illuminate built-in MacBook keyboard in low light"
+echo "Automatically illuminate built-in MacBook keyboard in low light ..."
 defaults write com.apple.BezelServices kDim -bool true
-echo "Turn off keyboard illumination when computer is not used for 5 minutes"
+
+echo "Turn off keyboard illumination when computer is not used for 5 minutes ..."
 defaults write com.apple.BezelServices kDimTime -int 300
 
-echo "Require password immediately after sleep or screen saver begins"
+echo "Require password immediately after sleep or screen saver begins ..."
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-echo "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
-defaults write com.apple.screencapture type -string "png"
+echo "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF) ..."
+defaults write com.apple.screencapture type -string "png ..."
 
-echo "Finder: show status bar"
+echo "Finder: show status bar ..."
 defaults write com.apple.finder ShowStatusBar -bool true
 
-echo "Use current directory as default search scope in Finder"
+echo "Use current directory as default search scope in Finder ..."
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-echo "Finder: allow text selection in Quick Look"
+echo "Finder: allow text selection in Quick Look ..."
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-echo "Disable disk image verification"
+echo "Disable disk image verification ..."
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-echo "Automatically open a new Finder window when a volume is mounted"
+echo "Automatically open a new Finder window when a volume is mounted ..."
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-echo "Disable the warning before emptying the Trash"
+echo "Disable the warning before emptying the Trash ..."
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-echo "Empty Trash securely by default"
+echo "Empty Trash securely by default ..."
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
-echo "Minimize windows into their application’s icon"
+echo "Minimize windows into their application’s icon ..."
 defaults write com.apple.dock minimize-to-application -bool true
 
-echo "Enable spring loading for all Dock items"
+echo "Enable spring loading for all Dock items ..."
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-echo "Speed up Mission Control animations"
+echo "Speed up Mission Control animations ..."
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
-echo "Group windows by application in Mission Control"
+echo "Group windows by application in Mission Control ..."
 defaults write com.apple.dock "expose-group-by-app" -bool true
 
-echo "Remove the auto-hiding Dock delay"
+echo "Remove the auto-hiding Dock delay ..."
 defaults write com.apple.Dock autohide-delay -float 0
 
-echo "Remove Notification Center from Menubar"
+echo "Remove Notification Center from Menubar ..."
 launchctl remove com.apple.notificationcenterui.agent
 
-echo "Run the screensaver if we're in the bottom-left hot corner."
+echo "Run the screensaver if we're in the bottom-left hot corner ..."
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-echo "Enable\"Go to recent space\" Gesture with 4 finger tap"
+echo "Enable\"Go to recent space\" Gesture with 4 finger tap ..."
 defaults write com.apple.dock double-tap-jump-back -bool TRUE
 
-echo "Disable automatically rearrange Spaces based on recent use"
+echo "Disable automatically rearrange Spaces based on recent use ..."
 defaults write com.apple.dock mru-spaces -bool false
 
 # Keyboard shortcuts
