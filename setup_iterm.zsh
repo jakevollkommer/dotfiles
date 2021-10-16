56a91366dd67d4f18a7f10c71c14d5a97b27d4d1
jakevollkommer <7663746+jakevollkommer@users.noreply.github.com>
2021-10-16 12:16:12 +1000

    track iterm2 profile

---
+++ setup_iterm.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting iTerm2 Setup >>>\n"
+
+echo "Setting preferences directory"
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
