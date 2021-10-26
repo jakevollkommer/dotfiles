b5756d6e7511e32983c75e8458c46a35eee17ae9
jakevollkommer <7663746+jakevollkommer@users.noreply.github.com>
2021-10-26 16:09:29 +1000

    Use dedicated windows iterm

--- setup_iterm.zsh
+++ setup_iterm.zsh
@@ -7,3 +7,6 @@ echo "Setting preferences directory"
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Set the animation of dedicated windows to be really fast
+defaults write com.googlecode.iterm2 HotkeyTermAnimationDuration -float 0.01
