0097dc1ae73b40ff269ab996e75205d090362309
jakevollkommer <7663746+jakevollkommer@users.noreply.github.com>
2021-11-19 12:55:22 +1000

    Add System Applications to dock

--- setup_dock.zsh
+++ setup_dock.zsh
@@ -2,13 +2,24 @@
 
 echo "\n<<< Setting up Dock >>>\n"
 
-add_to_dock() {
+add_app_to_dock() {
     echo "Adding $1 to Dock"
     defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$1.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 }
 
-for app in "Brave" "Spark" "Spotify" "Xcode" "iTerm"; do
-    add_to_dock $app
+add_system_app_to_dock() {
+    echo "Adding $1 to Dock"
+    defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/$1.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+}
+
+defaults write com.apple.dock persistent-apps -array
+
+for app in "System Preferences" "Messages"; do
+    add_system_app_to_dock $app
+done
+
+for app in "Brave Browser" "Spark" "Spotify" "Xcode" "iTerm" "Notion"; do
+    add_app_to_dock $app
 done
 
 killall Dock
