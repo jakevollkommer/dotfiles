21d895a9d03c823720a717e0923b7319af3b957b
jakevollkommer <7663746+jakevollkommer@users.noreply.github.com>
2021-11-19 12:24:53 +1000

    Install Rosetta for Silicon Macs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -8,6 +8,16 @@ if exists brew; then
 else
     echo "brew doesn't exist, continue with install"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Install Rosetta
+if [ -d "/usr/libexec/rosetta" ]; then
+    echo "Rosetta already exists, skipping install"
+else
+    echo "Rosetta not installed. Installing ..."
+    sudo softwareupdate --install-rosetta
 fi
 
 # Install taps/casks
